01e3c0ea5332d844e96421299acb7da6de8a2202
AkesSpur <crystallord2005@gmail.com>
2025-06-30 12:16:06 +0300

    log in users upon registration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -47,6 +47,8 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
+        Auth::login($user);
+
         return redirect(route('verification.notice'))->with('status', 'verification-link-sent');
     }
 }
