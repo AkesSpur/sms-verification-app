bb75562881c434092978c0a6ad6d1b01a299f319
AkesSpur <crystallord2005@gmail.com>
2025-06-30 23:58:23 +0300

    Changed the default from name

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -44,7 +44,7 @@ class AppServiceProvider extends ServiceProvider
             Config::set('mail.mailers.smtp.username', $mailSetting->username);
             Config::set('mail.mailers.smtp.password', $mailSetting->password);
             Config::set('mail.from.address', $mailSetting->email);
-            Config::set('mail.from.name', $generalSetting->site_name ?? 'SMS Verification App');
+            Config::set('mail.from.name', $generalSetting->site_name ?? 'BlizzSMS');
         }
 
 
