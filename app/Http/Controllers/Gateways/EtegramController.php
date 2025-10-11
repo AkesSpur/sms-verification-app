3e802cd600e43d447ce1ff47edf2bda52591c744
AkesSpur <crystallord2005@gmail.com>
2025-10-11 17:23:03 +0300

    test 16

--- app/Http/Controllers/Gateways/EtegramController.php
+++ app/Http/Controllers/Gateways/EtegramController.php
@@ -174,7 +174,7 @@ class EtegramController extends Controller
             'Authorization' => 'Bearer pk_live-8f369e47704244ff852dee6d3dc08163', // or use public key if required
             'Accept' => 'application/json',
         ])// NEW - CORRECT
-->get("https://api-checkout.etegram.com/api/transaction/verify-access-code/{$accessCode}");
+->patch("https://api-checkout.etegram.com/api/transaction/verify-access-code/{$accessCode}");
 
         
             // // PATCH request with custom SSL and timeout options
